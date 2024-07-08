e0c90a037815cef9e8c9a96980ad29147173b1b5
Mahadev Kharmate <mahadev.mk294@gmail.com>
2024-07-08 17:14:20 +0530

    prediction pipeline updated

--- src/mlProject/pipeline/prediction_pipeline.py
+++ src/mlProject/pipeline/prediction_pipeline.py
@@ -9,7 +9,7 @@ class PredictionPipeline:
     def __init__(self):
         self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
 
-    def prediction(self,data):
-        prediction=self.model.prediction(data)
-        
+    def predict(self, data):
+        prediction = self.model.predict(data)
+
         return prediction
